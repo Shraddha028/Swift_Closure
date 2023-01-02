bba6feb0608ff412a53a133ea5d8854c54f695ab
shraddha.sutar@accenture.com <shraddha.sutar@accenture.com>
2023-01-02 14:07:50 +0530

    Closure Example

--- ClosureExample.playground/Contents.swift
+++ ClosureExample.playground/Contents.swift
@@ -1,3 +1,12 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+
+let addition: (Int, Int) -> Int = {
+    (num1, num2) in
+    print("closure....")
+    return num1 + num2
+}
+
+print(addition(10,50))
